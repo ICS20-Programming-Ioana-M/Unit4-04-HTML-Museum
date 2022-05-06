a70d2290212b3ca92605b416104bcad6e4c1e867
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2022-05-06 14:18:41 +0000

    works :DDD

--- js/script.js
+++ js/script.js
@@ -9,22 +9,22 @@ function buttonClicked() {
   // If statement checks if user can go for free, with discount or pay normal price
   // user is less than 5 and more than 95 years old
   if(age >= 5 || age <= 95){
-    document.getElementById('answer').innerHTML = "You don't have to pay!!!"
+    document.getElementById('answer').innerHTML = "You don't have to pay!!!";
 
     // user goes on tuesday or wednesday and is within student age
-  } else if((day = "Teusday" || day = "Thurday") && (age >= 12 && age <= 21)){
-    document.getElementById('answer').innerHTML = "You get a student discount."
+  } else if((day == "Tuesday" || day == "Thurday") && (age >= 12 && age <= 21)) {
+    document.getElementById('answer').innerHTML = "You get a student discount.";
 
     // age is negative
   } else if(age < 0) {
-    document.getElementById('answer').innerHTML = "There's no way you're " + age + " years old."
+    document.getElementById('answer').innerHTML = "There's no way you're " + age + " years old.";
 
     // no day chosen
-  } else if(days = "nothing") {
-    document.getElementById('answer').innerHTML = "please pick a day"
+  } else if(days == "") {
+    document.getElementById('answer').innerHTML = "please pick a day";
 
     // literally everyone else
   } else {
-    document.getElementById('answer').innerHTML = "you pay regular price"
+    document.getElementById('answer').innerHTML = "you pay regular price";
   }
 }
