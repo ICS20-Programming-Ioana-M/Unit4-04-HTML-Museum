056f7947d0531c209f2cb59f4e03b9cc4a4a0544
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2022-05-07 01:28:31 +0000

    ...

--- js/script.js
+++ js/script.js
@@ -7,17 +7,16 @@ function buttonClicked() {
   let day = document.getElementById('days').value;
 
   // If statement checks if user can go for free, with discount or pay normal price
+  if(age < 0) {
+    document.getElementById('answer').innerHTML = "There's no way you're " + age + " years old."
+    
   // user is less than 5 and more than 95 years old
-  if(age <= 5 || age >= 95) {
+  } else if(age <= 5 || age >= 95) {
     document.getElementById('answer').innerHTML = "You don't have to pay!!!";
 
     // user goes on tuesday or wednesday and is within student age
   } else if(((day == "Tuesday") || (day == "Thursday")) || ((age >= 12) && (age <= 21))) {
     document.getElementById('answer').innerHTML = "You get a student discount.";
-
-    // age is negative
-  } else if(age < 0) {
-    document.getElementById('answer').innerHTML = "There's no way you're " + age + " years old.";
 
     // no day chosen
   } else if(days == "") {
